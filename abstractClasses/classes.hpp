ab24a7d9c482cc99e40da9dc1c0d56efcbd90c68
Wallace <piotr.walas.prywatny@gmail.com>
2024-03-12 22:56:49 +0100

    abstract Class

---
+++ abstractClasses/classes.hpp
@@ -0,0 +1,9 @@
+
+class A {
+    virtual int pure_virtual(){};
+};
+
+class AB {
+    virtual int pure_virtual(){};
+    int some_other_func(){return 0;};
+};
---
+++ abstractClasses/main.cpp
@@ -0,0 +1,10 @@
+#include "classes.hpp"
+#include <iostream>
+
+
+int main(int argc, char* argv[]){
+
+    A a;
+    AB ab;
+    return 0;
+}
